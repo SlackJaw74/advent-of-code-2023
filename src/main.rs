6869411649a85149d59fcd6a11437e19b201819e
Ben Wilson <ben.wilson@walmart.com>
2023-12-05 17:48:14 -0800

    chore: day 3 a

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day1;
 mod day2;
+mod day3;
 mod file_helper;
 
 use crate::day1::execute_day_1;
 use crate::day1::execute_day_1_b;
 use crate::day2::execute_day_2;
 use crate::day2::execute_day_2_b;
+use crate::day3::execute_day_3;
 
 fn main() {
     println!("Day 1a: {}", execute_day_1());
@@ -13,4 +15,6 @@ fn main() {
 
     println!("Day 2a: {}", execute_day_2());
     println!("Day 2b: {}", execute_day_2_b());
+
+    println!("Day 3a: {}", execute_day_3());
 }
