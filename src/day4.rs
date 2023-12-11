dcc5bd940aca7329ef9082d4951e2cb629e55448
Ben Wilson <benwil23@hotmail.com>
2023-12-11 00:02:18 -0800

    chore: day 4a initial

---
+++ src/day4.rs
@@ -0,0 +1,83 @@
+use std::{iter, collections::HashSet};
+
+use regex::Regex;
+
+use crate::file_helper::get_file_contents;
+
+pub fn execute_day_4_a(file_path: &str) -> i32 {
+    let lines = get_file_contents(file_path);
+    let total_score = lines
+        .iter()
+        .map(|line| Card::new(line))
+        .map(|x| x.get_card_score())
+        .sum();
+    return total_score;
+}
+
+#[derive(Default, Debug)]
+pub struct Card {
+    pub number: usize,
+    pub winning_numbers: HashSet<i32>,
+    pub ticket_numbers: HashSet<i32>,
+}
+impl Card {
+    pub fn new(line: &str) -> Self {
+        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        let lottery_card_regex = Regex::new(
+            r"Card (?<card_number>\d): (?<winning_numbers>[\d\s]+)\|(?<ticket_numbers>[\d\s]+)",
+        )
+        .unwrap();
+        let captures = lottery_card_regex.captures(line).unwrap();
+        let number = captures.name("card_number").map_or(0, |capture| {
+            capture.as_str().parse::<usize>().expect("bad card number")
+        });
+        let winning_numbers = captures
+            .name("winning_numbers")
+            .map(|capture| {
+                capture.as_str().trim().split(" ").map(|value| {
+                    value
+                        .to_string()
+                        .parse::<i32>()
+                        .expect("failed to parse value")
+                })
+            })
+            .expect("failed to get winning numbers")
+            .collect();
+        let ticket_numbers = captures
+            .name("ticket_numbers")
+            .map(|capture| {
+                capture.as_str().trim().split(" ").flat_map(|value| {
+                    let temp = value.to_string();
+                    println!("value {}", temp);
+                    temp.parse::<i32>()
+                })
+            })
+            .expect("boohoo")
+            .collect();
+        Card {
+            number,
+            winning_numbers,
+            ticket_numbers,
+        }
+    }
+    pub fn get_card_score(&self) -> i32 {
+        let ticket_numbers = &self.ticket_numbers;
+        let winning_numbers = &self.winning_numbers;
+        let card_score = ticket_numbers.intersection(winning_numbers).fold(1, |acc, _| acc + acc);
+        println!("card_score={}", card_score);
+        return card_score;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use insta::*;
+
+    #[test]
+    fn execute_day_4_a_mini() {
+        let result = execute_day_4_a("./input/day-4-test.txt");
+        assert_eq!(result, 13);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod file_helper;
 
 use crate::day1::execute_day_1;
@@ -9,6 +10,7 @@ use crate::day2::execute_day_2;
 use crate::day2::execute_day_2_b;
 use crate::day3::execute_day_3;
 use crate::day3::execute_day_3_b;
+use crate::day4::execute_day_4_a;
 
 fn main() {
     println!("Day 1a: {}", execute_day_1());
@@ -19,4 +21,6 @@ fn main() {
 
     println!("Day 3a: {}", execute_day_3());
     println!("Day 3b: {}", execute_day_3_b("./input/day-3.txt"));
+    
+    println!("Day 4a: {}", execute_day_4_a("./input/day-4.txt"));
 }
